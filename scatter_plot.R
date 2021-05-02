74e8c50ba0639573dd7b975cc2795f71e5e057f4
Jirapat Klaokliang <45891267+nattrio@users.noreply.github.com>
2021-05-03 02:26:24 +0700

    upload R

---
+++ scatter_plot.R
@@ -0,0 +1,21 @@
+setwd("C:/Users/Asus/Documents/R Project")
+getwd()
+
+library(tidyverse)
+library(ggpmisc)
+
+df <- read.csv("sample.csv")
+df$cut = factor(df$cut, levels = c("Fair", "Good", "Very Good", "Premium", "Ideal"))
+df$color = factor(df$color , levels = c("J", "I", "H", "G", "F", "E", "D"))
+df$clarity = factor(df$clarity, levels = c("I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1", "IF"))
+
+my.formula <- y ~ x
+
+ggplot(df, aes(carat, price)) +
+  geom_point(alpha = 0.3) +
+  geom_smooth(method = "lm", se = FALSE) + 
+  theme_minimal() +
+  stat_poly_eq(formula = my.formula, 
+               aes(label = paste(..rr.label.., sep = "~~~")), 
+               parse = TRUE) +
+  facet_wrap(~ color, ncol = 4)
