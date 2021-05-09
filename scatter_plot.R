794ecf2bc6cc03cd5a97071dbf7fd8471128ede7
Jirapat Klaokliang <jirapat.sit61@mail.kmutt.ac.th>
2021-05-09 19:01:58 +0700

    Update Hypothesis Testing

--- scatter_plot.R
+++ scatter_plot.R
@@ -7,7 +7,8 @@ library(ggpmisc)
 df <- read.csv("sample.csv")
 df$cut = factor(df$cut, levels = c("Fair", "Good", "Very Good", "Premium", "Ideal"))
 df$color = factor(df$color , levels = c("J", "I", "H", "G", "F", "E", "D"))
-df$clarity = factor(df$clarity, levels = c("I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1", "IF"))
+# df$clarity = factor(df$clarity, levels = c("I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1", "IF"))
+df$clarity = factor(df$clarity, levels = c( "IF","VVS1","VVS2", "VS1", "VS2","SI1","SI2","I1"))
 
 my.formula <- y ~ x
 
@@ -19,3 +20,5 @@ ggplot(df, aes(carat, price)) +
                aes(label = paste(..rr.label.., sep = "~~~")), 
                parse = TRUE) +
   facet_wrap(~ color, ncol = 4)
+
+ggplot(df, aes(x=carat, y=price, color=clarity)) + geom_point() +  theme_minimal()
